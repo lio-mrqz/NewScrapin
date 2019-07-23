2dcbdd21430cef8635d7b13ce546e3c5fe206935
Lio <lionell.marquez@gmail.com>
2019-07-23 18:22:17 -0400

    Does not scrape. No solutions yet

---
+++ server.js
@@ -0,0 +1,70 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "scraper";
+var collections = ["scrapedNews"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+  res.send("Hello world");
+});
+
+// Retrieve data from the db
+app.get("/all", function(req, res) {
+  // Find all results from the scrapedData collection in the db
+  db.scrapedNews.find({}, function(error, found) {
+    // Throw any errors to the console
+    if (error) {
+      console.log(error);
+    }
+    // If there are no errors, send the data to the browser as json
+    else {
+      res.json(found);
+    }
+  });
+});
+
+app.get("/scrape", function(req, res) {
+  axios.get("https://news.yahoo.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+    $(".title").each(function(i, element) {
+      var title = $(element).children("a").text();
+      var link = $(element).children("a").attr("href");
+
+      if (title && link) {
+        db.scrapedNews.insert({
+          title: title,
+          link: link
+        },
+        function(err, inserted) {
+          if (err) {
+            console.log(err);
+          }
+          else {
+            console.log(inserted);
+          }
+        })
+      }
+    });
+  });
+});
+
+
+// Listen on port 3000
+app.listen(3030, function() {
+  console.log("App running on port 3030!");
+});
